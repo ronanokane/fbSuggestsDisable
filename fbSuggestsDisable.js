617206132809b95b11e02af2c22d070d040db3ae
ronanokane <ronan.okane@gmail.com>
2021-02-13 00:02:12 +0000

    scrollHeight change is a more reliable way of knowing when to run our feed checks

--- fbSuggestsDisable.js
+++ fbSuggestsDisable.js
@@ -11,22 +11,27 @@ window.addEventListener('load', (event) => {
   removeSuggests();
  
   let countFeeds=document.evaluate("count(/html/body/div[1]/div/div[1]/div[1]/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div[3]/div/div[4]/div/div[@data-pagelet='FeedUnit_{n}'])",document, null, XPathResult.NUMBER_TYPE, null).numberValue;
- 
+  let prevScrollHeight;
+
   window.addEventListener('scroll', () => {
      const {
-         scrollTop,
-         scrollHeight,
-         clientHeight
+         scrollHeight
      } = document.documentElement;
+
+     prevScrollHeight= prevScrollHeight || scrollHeight;
     
-     if (scrollTop + clientHeight >= scrollHeight - 5){
+     if (scrollHeight > prevScrollHeight){
+       prevScrollHeight=scrollHeight;
+
+       // set interval must be used because sometimes there can be a pause of seconds before feeds load
+       // and we need a delay to keep checking when they have loaded.
        let x=setInterval(()=>{
          let nowCountFeeds=document.evaluate("count(/html/body/div[1]/div/div[1]/div[1]/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div[3]/div/div[4]/div/div[@data-pagelet='FeedUnit_{n}'])",document, null, XPathResult.NUMBER_TYPE, null).numberValue;
 
          // new news feeds
          if(nowCountFeeds>countFeeds){
            countFeeds=nowCountFeeds;
-           removeSuggests (); clearInterval(x);
+           removeSuggests(); clearInterval(x);
          }
        }, 500);
 
@@ -37,8 +42,7 @@ window.addEventListener('load', (event) => {
 });
 
 
-function removeSuggests()
-{
+function removeSuggests(){
  let results = document.evaluate("/html/body/div[1]/div/div[1]/div[1]/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div[3]/div/div[4]/div/div[@data-pagelet='FeedUnit_{n}']//span[contains(text(),'Suggested for you')]/../../../../../../../../../../../../../../../../.." ,document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null );
 
  for (let i = 0; i < results.snapshotLength; i++) {
