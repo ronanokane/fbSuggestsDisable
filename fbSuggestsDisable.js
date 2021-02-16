2be7706d269ab8bc3a90b30adc5736879a28a251
ronanokane <ronan.okane@gmail.com>
2021-02-16 22:18:54 +0000

    Checking scroll height instead of xpath count of feed elements simpler and works better

--- fbSuggestsDisable.js
+++ fbSuggestsDisable.js
@@ -10,35 +10,14 @@ window.addEventListener('load', (event) => {
   console.log("suggestions disabler on....");
   removeSuggests();
  
-  let countFeeds=document.evaluate("count(/html/body/div[1]/div/div[1]/div[1]/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div[3]/div/div[4]/div/div[@data-pagelet='FeedUnit_{n}'])",document, null, XPathResult.NUMBER_TYPE, null).numberValue;
-  let prevScrollHeight;
-
-  window.addEventListener('scroll', () => {
-     const {
-         scrollHeight
-     } = document.documentElement;
-
-     prevScrollHeight= prevScrollHeight || scrollHeight;
-    
-     if (scrollHeight > prevScrollHeight){
-       prevScrollHeight=scrollHeight;
-
-       // set interval must be used because sometimes there can be a pause of seconds before feeds load
-       // and we need a delay to keep checking when they have loaded.
-       let x=setInterval(()=>{
-         let nowCountFeeds=document.evaluate("count(/html/body/div[1]/div/div[1]/div[1]/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div[3]/div/div[4]/div/div[@data-pagelet='FeedUnit_{n}'])",document, null, XPathResult.NUMBER_TYPE, null).numberValue;
-
-         // new news feeds
-         if(nowCountFeeds>countFeeds){
-           countFeeds=nowCountFeeds;
-           removeSuggests(); clearInterval(x);
-         }
-       }, 500);
-
-       // get us out if no new news items
-       setTimeout(()=>{clearInterval(x);},8000);
-     }
-  });
+  let prevScrollHeight= document.documentElement.scrollHeight;
+  
+	setInterval(()=>{  
+    if(prevScrollHeight < document.documentElement.scrollHeight){
+      prevScrollHeight=document.documentElement.scrollHeight;
+      removeSuggests();
+    }
+  }, 300);
 });
 
 
