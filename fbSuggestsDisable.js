1ce91e4ea162712e62cf920492fd8c533c68dd54
ronanokane <ronan.okane@gmail.com>
2021-04-20 14:12:48 +0100

    tidied up a bit

--- fbSuggestsDisable.js
+++ fbSuggestsDisable.js
@@ -9,25 +9,19 @@
 window.addEventListener('load', (event) => {
   console.log("suggestions disabler on....");
   removeSuggests();
- 
+  
   let prevScrollHeight= document.documentElement.scrollHeight;
   
-	setInterval(()=>{  
-    if(prevScrollHeight < document.documentElement.scrollHeight){
-      prevScrollHeight=document.documentElement.scrollHeight;
-      removeSuggests();
-    }
-  }, 300);
+	setInterval(()=>{
+    prevScrollHeight < document.documentElement.scrollHeight && (prevScrollHeight=document.documentElement.scrollHeight) && removeSuggests();
+  }, 200);
 });
 
 
 function removeSuggests(){
- let results = document.evaluate("/html/body/div[1]/div/div[1]/div[1]/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div[3]/div/div[4]/div/div[@data-pagelet='FeedUnit_{n}']//span[contains(text(),'Suggested for you')]/../../../../../../../../../../../../../../../../.." ,document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null );
+ let results = document.evaluate("/html/body/div[1]/div/div[1]/div[1]/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div[3]/div/div[4]/div/div[@data-pagelet='FeedUnit_{n}']//span[starts-with(text(),'Sugge')]/../../../../../../../../../../../../../../../../.." ,document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null );
 
  for (let i = 0; i < results.snapshotLength; i++) {
-   if(results.snapshotItem(i).style.display!='none'){
-     results.snapshotItem(i).style.display='none';
-     console.log("removed a suggest..");
-   }
+   results.snapshotItem(i).style.display!=='none' && (results.snapshotItem(i).style.display='none');
  }
 }
