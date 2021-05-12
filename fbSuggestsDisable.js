1051be4361dfc6bcecf9942338ddb856c0dc540c
ronanokane <ronan.okane@gmail.com>
2021-05-12 01:31:48 +0100

    remove actual suggest nodes rather than set display to none. Less overhead.

--- fbSuggestsDisable.js
+++ fbSuggestsDisable.js
@@ -13,15 +13,18 @@ window.addEventListener('load', (event) => {
   let prevScrollHeight= document.documentElement.scrollHeight;
   
 	setInterval(()=>{
-    prevScrollHeight < document.documentElement.scrollHeight && (prevScrollHeight=document.documentElement.scrollHeight) && removeSuggests();
+    if(prevScrollHeight < document.documentElement.scrollHeight){ 
+      prevScrollHeight=document.documentElement.scrollHeight;
+      removeSuggests();
+    }
   }, 200);
 });
-
 
 function removeSuggests(){
  let results = document.evaluate("/html/body/div[1]/div/div[1]/div[1]/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div[3]/div/div[4]/div/div[@data-pagelet='FeedUnit_{n}']//span[starts-with(text(),'Sugge')]/../../../../../../../../../../../../../../../../.." ,document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null );
 
- for (let i = 0; i < results.snapshotLength; i++) {
-   results.snapshotItem(i).style.display!=='none' && (results.snapshotItem(i).style.display='none');
+ for (let i = 0; i < results.snapshotLength; i++){
+   results.snapshotItem(i).remove();
+   console.log("removed....");
  }
 }
