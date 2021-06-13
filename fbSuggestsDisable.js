0b580e71cba2304a04407bc0dff64726849ed0d5
ronanokane <ronan.okane@gmail.com>
2021-06-13 22:09:09 +0100

    Tidied up a bit

--- fbSuggestsDisable.js
+++ fbSuggestsDisable.js
@@ -8,6 +8,7 @@
 
 window.addEventListener('load', (event) => {
   var timeElapsed=0, feedsLocation,
+      
 	x=setInterval(()=> {
     
     if((timeElapsed+=200)>=5000){clearInterval(x); return;}
@@ -16,10 +17,10 @@ window.addEventListener('load', (event) => {
         console.log("suggestions disabler on....");
       	clearInterval(x);
         
-        var mutationObserver = new MutationObserver(function(mutations) {
-          mutations.forEach(function(mutation) {
-            if(mutation.addedNodes.length>0 && mutation.addedNodes[0].attributes?.getNamedItem("data-pagelet"))
-  							document.evaluate("./div/div/div/div/div/div/div/div/div/div/div[2]/div/div[1]/div/div//span[starts-with(text(),'Sugge')]", mutation.addedNodes[0], null, XPathResult.ANY_TYPE).iterateNext() 
+        var mutationObserver = new MutationObserver((mutations)=> {
+          mutations.forEach((mutation)=> {
+            mutation.addedNodes.length>0 && mutation.addedNodes[0].attributes?.getNamedItem("data-pagelet")
+  							&& document.evaluate("./div/div/div/div/div/div/div/div/div/div/div[2]/div/div[1]/div/div//span[starts-with(text(),'Sugge')]", mutation.addedNodes[0], null, XPathResult.ANY_TYPE).iterateNext() 
     						&& (mutation.addedNodes[0].style.display='none') 
     						&& console.log("suggest removed");
           });
