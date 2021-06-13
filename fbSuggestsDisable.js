366c2da4a6e673efdbcfe5d15d0100dac51ad169
ronanokane <ronan.okane@gmail.com>
2021-06-13 20:50:06 +0100

    Support for MutationObserver instead of polling.

--- fbSuggestsDisable.js
+++ fbSuggestsDisable.js
@@ -1,30 +1,39 @@
 // ==UserScript==
-// @name        Facebook_suggestions_disabler
+// @name        Facebook_suggestions_disabler v1.2
 // @description	Remove the annoying facebook suggestions
 // @author      Ronan O' Kane
 // @include     https://www.facebook.com/
-// @version     v1.0
+// @version     v1.2
 // ==/UserScript==
 
 window.addEventListener('load', (event) => {
-  console.log("suggestions disabler on....");
-  removeSuggests();
-  
-  let prevScrollHeight= document.documentElement.scrollHeight;
-  
-	setInterval(()=>{
-    if(prevScrollHeight < document.documentElement.scrollHeight){ 
-      prevScrollHeight=document.documentElement.scrollHeight;
-      removeSuggests();
-    }
-  }, 200);
+  var timeElapsed=0, feedsLocation,
+      
+	x=setInterval(()=> {
+    
+    if((timeElapsed+=200)>=5000){clearInterval(x); return;}
+    
+    if((feedsLocation=document.evaluate("/html/body/div[1]/div/div[1]/div[1]/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div[3]/div/div[4]/div" ,document, null, XPathResult.ANY, null ).iterateNext())){
+        console.log("suggestions disabler on....");
+      	clearInterval(x);
+        
+        var mutationObserver = new MutationObserver(function(mutations) {
+          mutations.forEach(function(mutation) {
+            if(mutation.addedNodes.length>0 && mutation.addedNodes[0].attributes?.getNamedItem("data-pagelet"))
+  							document.evaluate("./div/div/div/div/div/div/div/div/div/div/div[2]/div/div[1]/div/div//span[starts-with(text(),'Sugge')]", mutation.addedNodes[0], null, XPathResult.ANY_TYPE).iterateNext() 
+    						&& (mutation.addedNodes[0].style.display='none') 
+    						&& console.log("suggest removed");
+          });
+        });
+
+        mutationObserver.observe(feedsLocation, {
+          attributes: false,
+          characterData: false,
+          childList: true,
+          subtree: true,
+          attributeOldValue: false,
+          characterDataOldValue: false
+        });        
+    } 
+  }, 200);        
 });
-
-function removeSuggests(){
- let results = document.evaluate("/html/body/div[1]/div/div[1]/div[1]/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div[3]/div/div[4]/div/div[@data-pagelet='FeedUnit_{n}']//span[starts-with(text(),'Sugge')]/../../../../../../../../../../../../../../../../.." ,document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null );
-
- for (let i = 0; i < results.snapshotLength; i++){
-   results.snapshotItem(i).remove();
-   console.log("removed....");
- }
-}
