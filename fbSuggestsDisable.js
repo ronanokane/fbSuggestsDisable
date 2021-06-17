cce3b71245bcf55421c897b6f1156e0e879a0196
ronanokane <ronan.okane@gmail.com>
2021-06-17 08:28:26 +0100

    Tidied up a bit

--- fbSuggestsDisable.js
+++ fbSuggestsDisable.js
@@ -8,7 +8,6 @@
 
 window.addEventListener('load', (event) => {
   var timeElapsed=0, feedsLocation, x=setInterval(()=> {
-    
     if((timeElapsed+=200)>=5000){
       clearInterval(x); 
       return;
@@ -20,9 +19,9 @@ window.addEventListener('load', (event) => {
         
         var mutationObserver = new MutationObserver((mutations)=>
           mutations.forEach((mutation)=> mutation.addedNodes.length>0 && mutation.addedNodes[0].attributes?.getNamedItem("data-pagelet")
-  							&& document.evaluate("./div/div/div/div/div/div/div/div/div/div/div[2]/div/div[1]/div/div//span[starts-with(text(),'Sugge')]", mutation.addedNodes[0], null, XPathResult.ANY_TYPE).iterateNext() 
-    						&& (mutation.addedNodes[0].style.display='none') 
-    						&& console.log("suggest removed"))
+              && document.evaluate("./div/div/div/div/div/div/div/div/div/div/div[2]/div/div[1]/div/div//span[starts-with(text(),'Sugge')]", mutation.addedNodes[0], null, XPathResult.ANY_TYPE).iterateNext() 
+              && (mutation.addedNodes[0].style.display='none') 
+              && console.log("suggest removed"))
         );
 
         mutationObserver.observe(feedsLocation, {
