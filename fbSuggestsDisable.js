b9723f060d724705de5b91279d68fbe2043815b0
ronanokane <36424410+ronanokane@users.noreply.github.com>
2021-08-04 02:07:52 +0100

    Tidied up.

--- fbSuggestsDisable.js
+++ fbSuggestsDisable.js
@@ -9,11 +9,10 @@
 
 new MutationObserver(function(mutat) {
 	let feedsLocation=document.querySelector('div[role="feed"]');
-  
+	
 	if(feedsLocation){
 		this.disconnect();
 		console.log("suggestions disabler on....");
-
 		new MutationObserver((mutations)=>
 		    mutations.forEach((mutation)=> mutation.addedNodes.length>0 && mutation.addedNodes[0].attributes?.getNamedItem("data-pagelet")
 			    && document.evaluate("./div/div/div/div/div/div/div/div/div/div/div/div/div/div/div//span[starts-with(text(),'Sugge')]", mutation.addedNodes[0], null, XPathResult.ANY_TYPE).iterateNext() 
