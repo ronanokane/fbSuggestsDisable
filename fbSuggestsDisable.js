a37a79eefd0a50e736dfcdb62c8f185882113dc3
ronanokane <36424410+ronanokane@users.noreply.github.com>
2021-08-04 02:02:50 +0100

    Tidied up.

--- fbSuggestsDisable.js
+++ fbSuggestsDisable.js
@@ -11,15 +11,15 @@ new MutationObserver(function(mutat) {
 	let feedsLocation=document.querySelector('div[role="feed"]');
   
 	if(feedsLocation){
-        this.disconnect();
-        console.log("suggestions disabler on....");
+		this.disconnect();
+		console.log("suggestions disabler on....");
 
-        new MutationObserver((mutations)=>
-            mutations.forEach((mutation)=> mutation.addedNodes.length>0 && mutation.addedNodes[0].attributes?.getNamedItem("data-pagelet")
-                    && document.evaluate("./div/div/div/div/div/div/div/div/div/div/div/div/div/div/div//span[starts-with(text(),'Sugge')]", mutation.addedNodes[0], null, XPathResult.ANY_TYPE).iterateNext() 
-                    && (mutation.addedNodes[0].style.display='none') 
-                    && console.log("suggest removed"))
-        ).observe(feedsLocation, { childList: true });        
-    } 
+		new MutationObserver((mutations)=>
+		    mutations.forEach((mutation)=> mutation.addedNodes.length>0 && mutation.addedNodes[0].attributes?.getNamedItem("data-pagelet")
+			    && document.evaluate("./div/div/div/div/div/div/div/div/div/div/div/div/div/div/div//span[starts-with(text(),'Sugge')]", mutation.addedNodes[0], null, XPathResult.ANY_TYPE).iterateNext() 
+			    && (mutation.addedNodes[0].style.display='none') 
+			    && console.log("suggest removed"))
+		).observe(feedsLocation, { childList: true });        
+    	} 
 }).observe(document, {subtree: true,
                       childList: true});
