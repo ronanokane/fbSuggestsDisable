7de5a8c916fdc209489a23da96178ae1a5ddc003
Ronan O' Kane <ronan.okane@gmail.com>
2023-02-06 11:47:21 +0000

    Update fbSuggestsDisable.js

--- fbSuggestsDisable.js
+++ fbSuggestsDisable.js
@@ -7,17 +7,15 @@
 // @version     v1.2
 // ==/UserScript==
 
-new MutationObserver(function(mutat) {
-	let feedsLocation=document.evaluate('//div[./span/@id="ssrb_feed_start"]/div[1]/div[1]',document, null, XPathResult.ANY_TYPE).iterateNext();
-	if(feedsLocation){
-		this.disconnect();
-		console.log("suggestions disabler on....");
-		new MutationObserver((mutations)=>
-		    mutations.forEach((mutation)=> mutation.addedNodes.length>0
-			    && document.evaluate(".//span[starts-with(text(),'Sugge')]", mutation.addedNodes[0], null, XPathResult.ANY_TYPE).iterateNext()
-			    && (mutation.addedNodes[0].style.display='none') 
-			    && console.log("suggest removed"))
-		).observe(feedsLocation, { childList: true });        
-    	} 
-}).observe(document, {subtree: true,
-                      childList: true});
+let feedsLocation=document.evaluate('//div[./span/@id="ssrb_feed_start"]/div[1]/div[1]',document, null, XPathResult.ANY_TYPE).iterateNext()
+if(!feedsLocation){
+    alert('Html Structure changed... update script')
+    return;
+}
+console.log("suggestions disabler on....");
+
+new MutationObserver((mutations)=>mutations.forEach((mutation)=> mutation.addedNodes.length>0
+    && document.evaluate(".//span[starts-with(text(),'Sugge')]", mutation.addedNodes[0], null, XPathResult.ANY_TYPE).iterateNext()
+    && (mutation.addedNodes[0].style.display='none')
+    && console.log("suggest removed"))
+).observe(feedsLocation, { childList: true });
