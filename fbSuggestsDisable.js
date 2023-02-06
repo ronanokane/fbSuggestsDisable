d1c39a7536a1b90a7cfdcfcff249630a7ca6a2ca
Ronan O' Kane <ronan.okane@gmail.com>
2023-02-06 20:46:18 +0000

    Update fbSuggestsDisable.js

--- fbSuggestsDisable.js
+++ fbSuggestsDisable.js
@@ -2,14 +2,14 @@
 // @name        fbSuggestsDisable
 // @description	Remove the annoying facebook suggestions
 // @author      Ronan O' Kane
-// @include     https://www.facebook.com/
+// @match     https://www.facebook.com/
 // @run-at document-end
 // @version     v1.2
 // ==/UserScript==
 
-let feedsLocation=document.evaluate('//div[./span/@id="ssrb_feed_start"]/div[1]/div[1]',document, null, XPathResult.ANY_TYPE).iterateNext()
+let feedsLocation=document.evaluate('//div[./span/@id="ssrb_feed_start"]/div[1]/div[1]',document, null, XPathResult.ANY_TYPE).iterateNext();
 if(!feedsLocation){
-    alert('Html Structure changed... update script')
+    alert('Html Structure changed... update script');
     return;
 }
 console.log("suggestions disabler on....");
