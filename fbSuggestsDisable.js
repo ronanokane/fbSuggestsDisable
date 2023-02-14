273be8e92ad83bbcf164008b387e2eab5ce9f27b
Ronan O' Kane <ronan.okane@gmail.com>
2023-02-14 02:47:41 +0000

    Facebook html structure change.

--- fbSuggestsDisable.js
+++ fbSuggestsDisable.js
@@ -7,7 +7,7 @@
 // @version     v1.2
 // ==/UserScript==
 
-let feedsLocation=document.evaluate('//div[./span/@id="ssrb_feed_start"]/div[1]/div[1]',document, null, XPathResult.ANY_TYPE).iterateNext();
+let feedsLocation=document.evaluate('//div[./h3/text()="News Feed posts"]/div[1]',document, null, XPathResult.ANY_TYPE).iterateNext();
 if(!feedsLocation){
     alert('Html Structure changed... update fbSuggestsDisable');
     return;
@@ -15,7 +15,7 @@ if(!feedsLocation){
 console.log("suggestions disabler on....");
 
 new MutationObserver((mutations)=>mutations.forEach((mutation)=> mutation.addedNodes.length>0
-    && document.evaluate(".//span[starts-with(text(),'Sugge')]", mutation.addedNodes[0], null, XPathResult.ANY_TYPE).iterateNext()
+    && document.evaluate(".//span[starts-with(text(),'Suggested')]", mutation.addedNodes[0], null, XPathResult.ANY_TYPE).iterateNext()
     && (mutation.addedNodes[0].style.display='none')
     && console.log("suggest removed"))
 ).observe(feedsLocation, { childList: true });
